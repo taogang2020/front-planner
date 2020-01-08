f05da17b2db3c69d71192f1e321bf132aac5e37c
sunruijie <1193300787@qq.com>
2020-01-08 13:56:28 +0800

    文件上传限制大小

--- src/components/customer/PersonalRegister.vue
+++ src/components/customer/PersonalRegister.vue
@@ -39,11 +39,11 @@
         <p class="title">请上传相关证件</p>
         <div class="ImgBox clear">
           <div class="file idCardPositive fl">
-            <van-uploader v-model="positiveFileList" :max-count="1" :after-read="uploadPositive" :before-delete="delPositive"/>
+            <van-uploader v-model="positiveFileList" accept=".jpg,.jpeg,.png,.pdf,.JPG,.JPEG,.PDF" :before-read='beforeUpload' :max-count="1" :after-read="uploadPositive" :before-delete="delPositive"/>
             <p>身份证正面</p>
           </div>
           <div class="file idCardReverse fr">
-            <van-uploader v-model="reverseFileList" :max-count="1" :after-read="uploadNegative" :before-delete="delNegative" />
+            <van-uploader v-model="reverseFileList" accept=".jpg,.jpeg,.png,.pdf,.JPG,.JPEG,.PDF" :before-read='beforeUpload' :max-count="1" :after-read="uploadNegative" :before-delete="delNegative" />
             <p>身份证反面</p>
           </div>
         </div>
@@ -202,6 +202,15 @@ export default {
     uploadNegative(file) {
       var _this = this;
       _this.uploadFile(file,4);
+    },
+    // 上传图片验证
+    beforeUpload(file) {
+      var _this = this;
+      const isLt2M = file.size / 1024 / 1024 < 2;
+      if (!isLt2M) {
+        _this.$toast("上传文件不能超过2MB");
+      }
+      return isLt2M;
     },
     // 上传图片
     uploadFile(params,fileType) {
